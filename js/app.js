6320a99850a8bab8ffc34889401629f2f613e45b
NathanBilleau <nbilleau@gmail.com>
2017-08-14 23:27:23 +0200

    pass field added

--- js/app.js
+++ js/app.js
@@ -7,13 +7,19 @@ const path = require('path')
 class App extends React.Component {
   render() {
     return(
-      <div className="main">
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-        <Column />
-      </div>
+
+        <div className="main">
+
+          <div className="modalContainer">
+            <Password />
+          </div>
+
+          <Column />
+          <Column />
+          <Column />
+          <Column />
+          <Column />
+        </div>
     )
   }
 }
@@ -153,6 +159,38 @@ class Item extends React.Component {
   }
 }
 
+
+
+class Password extends React.Component {
+
+  constructor(){
+    super()
+    this.state = {
+      pass: '123456'
+    }
+  }
+
+  submit(){
+    let prompt = document.getElementById('passPrompt').value
+
+    if(prompt === this.state.pass){
+      console.log('correct');
+    }
+    else{
+      console.log('incorrect');
+    }
+  }
+
+  render(){
+    return (
+      <div className="modal">
+        <h1>Mot de passe</h1>
+        <input type="text" id="passPrompt" />
+        <input type="button" onClick={() => this.submit()} value="OK"/>
+      </div>
+    )
+  }
+}
 
 
 
